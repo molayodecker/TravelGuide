c049e18cf8f20eb55e7bae1d941418ded587fbd3
Arthur Decker <molayodecker@gmail.com>
2017-09-04 03:09:59 -0400

    Compass not serving scss

--- kodaktravel_node/gulpfile.js
+++ kodaktravel_node/gulpfile.js
@@ -1,5 +1,77 @@
-var gulp = require('gulp');
 
-gulp.task('default', function(){
-	console.log('gulp works!');
+//////////////////////////////////////
+///////// REQUIRE  //////////////////
+////////////////////////////////////
+var gulp = require('gulp'),
+uglify = require('gulp-uglify'),
+compass = require('gulp-compass'),
+browserSync = require('browser-sync'),
+reload = browserSync.reload,
+autoprefixer = require('gulp-autoprefixer');
+plumber = require('gulp-plumber');
+rename = require('gulp-rename');
+
+//////////////////////////////////////
+///////// SCRIPT-TASK ///////////////
+////////////////////////////////////  
+
+gulp.task('scripts', function(){
+	gulp.src(['app/js/**/*.js', 'app/js/**/*.min.js'])
+	.pipe(plumber())
+	.pipe(rename({suffix:'.min'}))
+	.pipe(uglify())
+	.pipe(gulp.dest('app/js/'));
 });
+
+ //////////////////////////////////////
+//////////////COMPASS / SCSS TASK ////
+/////////////////////////////////////  
+
+gulp.task('compass', function(){
+	gulp.src('app/sass/style.sass')
+	.pipe(plumber())
+	.pipe(compass({
+		config_file: './config.rb',
+		css: 'app/css',
+		sass: 'app/sass',
+		require: ['susy']
+	}))
+	.pipe(autoprefixer('last 2 versions'))
+	.pipe(gulp.dest('app/css/'))
+	.pipe(reload({stream:true}));
+});
+
+ //////////////////////////////////////
+//////////////HTML TASK /////
+///////////////////////////////////// 
+gulp.task('html', function(){
+	gulp.src('app/**/*.html');
+});
+
+ //////////////////////////////////////
+//////////////Browser-Sync task ////
+/////////////////////////////////////  
+gulp.task('browser-sync',function(){
+	browserSync({
+		server:{
+			baseDir: "./app/"
+		}
+	});
+});
+
+
+//////////////////////////////////////
+///////// WATCH-TASK ////////////////
+//////////////////////////////////// 
+
+gulp.task('watch', function(){
+	gulp.watch('app/js/**/*.js',['scripts']);
+	gulp.watch('app/scss/**/*.scss',['compass']);
+	gulp.watch('app/**/*.html',['html']);
+});
+
+//////////////////////////////////////
+///////// DEFAULT-TASK //////////////
+////////////////////////////////////  
+
+gulp.task('default', ['scripts','compass','html','browser-sync','watch']);
